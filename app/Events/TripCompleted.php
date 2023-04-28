14f34d216d70ed419b2193608da2d4f0550b665e
Ye Yint Kyaw <yeyintkyaw300@gmail.com>
2023-04-28 16:19:00 +0630

    implements ShouldBroadCast in events

--- app/Events/TripCompleted.php
+++ app/Events/TripCompleted.php
@@ -12,7 +12,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class TripCompleted
+class TripCompleted implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
